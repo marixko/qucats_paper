0aa042a73a662a6c356b22fce681a231638f0346
mariko <marixko@gmail.com>
2023-12-30 18:25:43 -0300

    fixed bug in the lambda array order

--- codes/utils/correct_extinction.py
+++ codes/utils/correct_extinction.py
@@ -17,13 +17,24 @@ def correction(data):
 
     # Calculando a extinção em outros comprimentos de onda
     # Utilizando a lei de extinção de Cardelli, Clayton & Mathis.
-    Lambdas = np.array([1549.02, 2304.74,                                                        # FUV, NUV
-                        3536, 3770, 3940, 4094, 4292, 4751, 5133, 6258, 6614, 7690, 8611, 8831,  
-                        33526.00, 46028.00])
+    # lambdas = np.array([1549.02, 2304.74,                                                        # FUV, NUV
+    #                     3536, 3770, 3940, 4094, 4292, 4751, 5133, 6258, 6614, 7690, 8611, 8831,  
+    #                     33526.00, 46028.00])
+    
+    lambdas_dict = {'FUVmag': 1549.02, 'NUVmag': 2304.74,
+                    'u_PStotal': 3536, 'J0378_PStotal': 3770, 'J0395_PStotal': 3940, 'J0410_PStotal': 4094,
+                    'J0430_PStotal': 4292, 'g_PStotal': 4751, 'J0515_PStotal': 5133, 'r_PStotal': 6258,
+                    'J0660_PStotal': 6614, 'i_PStotal': 7690, 'J0861_PStotal': 8611, 'z_PStotal': 8831,
+                    'W1': 33526, 'W2': 46028}
+    
+    lambdas = []
+    for mag in feat:
+        lambdas.append(lambdas_dict[mag])
+    lambdas = np.array(lambdas, dtype=float)
 
     Extinctions = []
     for i in range(len(AV)):
-        Extinctions.append(extinction.ccm89(Lambdas, AV[i], 3.1))
+        Extinctions.append(extinction.ccm89(lambdas, AV[i], 3.1))
 
     Extinction_DF = pd.DataFrame(Extinctions, columns=feat)
     chunk = chunk.reset_index(drop=True)
