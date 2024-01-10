762580053ada989663a63a7bef96cebfc751ac54
Raquel Ruiz Valença <vruizraquel@usp.br>
2024-01-10 20:43:35 -0300

    minor code adjustments

--- codes/validation/BNN/misc/network.py
+++ codes/validation/BNN/misc/network.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import tensorflow_probability as tfp; tfd = tfp.distributions
+
+
 Input = tf.keras.layers.Input
 Dense = tf.keras.layers.Dense
 BatchNormalization = tf.keras.layers.BatchNormalization
@@ -8,10 +10,10 @@ DenseVariational = tfp.layers.DenseVariational
 MixtureNormal = tfp.layers.MixtureNormal
 
 # ML configs #
-Units = 196 # Units in the network
-Num_Layers = 3 # Number of hidden layers in the network
-Epochs = 600 # Epochs to train
-Batch_Size = 1024 # Batch size
+units = 196 # Units in the network
+n_layers = 3 # Number of hidden layers in the network
+epochs = 600 # Epochs to train
+batch_size = 1024 # Batch size
 
 # MDN configs #
 num_components = 7 # Number of components in the mixture
@@ -19,8 +21,8 @@ event_shape = [1] # Shape of the target
 lr = 0.001
 clipvalue = 0.5
 clipnorm = 0.5
-Activation = tf.keras.layers.LeakyReLU()
-Opt = tf.keras.optimizers.Adam(lr=lr, clipvalue=clipvalue, clipnorm=clipnorm)
+activation = tf.keras.layers.LeakyReLU()
+opt = tf.keras.optimizers.Adam(lr=lr, clipvalue=clipvalue, clipnorm=clipnorm)
 
 # Output config #
 # Required input size for the mixture layer (num_components x event_shape x 3)
@@ -28,7 +30,7 @@ params_size = tfp.layers.MixtureNormal.params_size(num_components, event_shape)
 
 
 def negloglik(y, p_y):
-        return -p_y.log_prob(y)
+    return -p_y.log_prob(y)
 
 
 def prior(kernel_size, bias_size=0, dtype=None):
@@ -55,32 +57,25 @@ def posterior(kernel_size, bias_size, dtype=None):
         ])
 
 
-def Dense_Variational(Train_Sample_Size=None, Number_of_Features=None):
+def Dense_Variational(train_sample_size=None, n_features=None):
     tf.keras.backend.clear_session()
     
-    Input_Layer = Input(shape=(int(Number_of_Features),))
+    Input_Layer = Input(shape=(int(n_features),))
+    l = DenseVariational(units, make_posterior_fn=posterior, make_prior_fn=prior,
+                         kl_weight=1/train_sample_size, kl_use_exact=True, activation=activation
+                         )(Input_Layer)
+    l = BatchNormalization()(l)
 
-    for i in range(Num_Layers):
-        
-        if i == 0:
-            l = DenseVariational(
-                Units, make_posterior_fn=posterior, make_prior_fn=prior,
-                kl_weight=1/Train_Sample_Size, kl_use_exact=True, activation=Activation
-                )(Input_Layer)
-            l = BatchNormalization()(l)
-            
-        else:
-            l = DenseVariational(
-                Units, make_posterior_fn=posterior, make_prior_fn=prior,
-                kl_weight=1/Train_Sample_Size, kl_use_exact=True, activation=Activation
-                )(l)
-            l = BatchNormalization()(l)
+    for _ in range(n_layers-1):
+        l = DenseVariational(units, make_posterior_fn=posterior, make_prior_fn=prior,
+                             kl_weight=1/train_sample_size, kl_use_exact=True, activation=activation
+                             )(l)
+        l = BatchNormalization()(l)
 
-    l = Dense(params_size, activation=Activation)(l)
-
+    l = Dense(params_size, activation=activation)(l)
     out = MixtureNormal(num_components, event_shape)(l)
 
     model = tf.keras.models.Model(inputs=Input_Layer, outputs=out)
-    model.compile(optimizer=Opt, loss=negloglik)
+    model.compile(optimizer=opt, loss=negloglik)
 
     return model
